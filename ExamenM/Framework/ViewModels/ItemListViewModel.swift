65a80bfb2eb2cc78f369e838704d4b524c9c08d6
Ana Paola Hernández Hernández <pauchas836@gmail.com>
2025-11-27 18:05:11 -0600

    add flags and improve UI

--- ExamenM/Framework/ViewModels/ItemListViewModel.swift
+++ ExamenM/Framework/ViewModels/ItemListViewModel.swift
@@ -3,13 +3,35 @@ import Combine
 
 class ItemListViewModel: ObservableObject {
     @Published var items = [ItemBase]()
+    @Published var searchText = ""
+    
     var listReq: ItemListRequirementProtocol
     var detailReq: ItemDetailRequirementProtocol
+    var userRepo: UserServiceProtocol
+    
+    var filteredItems: [ItemBase] {
+        if searchText.isEmpty {
+            return items
+        } else {
+            return items.filter { $0.ref.name.lowercased().contains(searchText.lowercased()) }
+        }
+    }
     
     init(listReq: ItemListRequirementProtocol = ItemListRequirement.shared,
-         detailReq: ItemDetailRequirementProtocol = ItemDetailRequirement.shared) {
+         detailReq: ItemDetailRequirementProtocol = ItemDetailRequirement.shared,
+         userRepo: UserServiceProtocol = UserRepository.shared) {
         self.listReq = listReq
         self.detailReq = detailReq
+        self.userRepo = userRepo
+        
+        // Cargar última búsqueda
+        if let last = userRepo.getLastViewedCountry() {
+            self.searchText = last
+        }
+    }
+    
+    func saveLastViewed(country: String) {
+        userRepo.setLastViewedCountry(country)
     }
     
     @MainActor
